eacb615983db1fc37c9fd2e688fdc189a5420508
Adnir Andrade <adnir_andrade@hotmail.com>
2023-04-08 18:23:44 -0300

    O.O. Organized, now discovering how to instantiate.

---
+++ pages/log-in/test.js
@@ -0,0 +1,8 @@
+// // import FormValidator from "./log-in-controller.js";
+// import FormValidator from "./log-in-controller.js";
+
+// const validator = new FormValidator();
+
+import exports from "./log-in-controller.js";
+
+const validator = exports;
