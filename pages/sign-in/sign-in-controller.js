48864dac1f8f3764f1a8ffbbe351c0669d03b532
Adnir Andrade <adnir_andrade@hotmail.com>
2023-04-08 13:23:54 -0300

    sign-in can now completely verify password

--- pages/sign-in/sign-in-controller.js
+++ pages/sign-in/sign-in-controller.js
@@ -7,12 +7,20 @@ form.addEventListener("submit", (event) => {
   const password = document.getElementById("password").value;
   const confirmPassword = document.getElementById("confirmPassword").value;
 
+  if (!validateEmail(email) ?? false) {
+    window.alert("Please, insert a valid e-mail address.");
+    return;
+  }
+
   if (!validatePassword(password) || !validatePassword(confirmPassword)) {
     window.alert("Please, insert a valid password.");
     return;
   }
 
-  // TODO: Compare passwords to check if they match
+  if (!isEqual(password, confirmPassword)) {
+    window.alert("Password does not match. Please, try again.");
+    return;
+  }
 
   form.submit();
   window.location.href = "/pages/select-table/select-table.html";
@@ -23,10 +31,14 @@ function validatePassword(password) {
   else return false;
 }
 
-// const validateEmail = (email) => {
-//   return String(email)
-//     .toLowerCase()
-//     .match(
-//       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-//     );
-// };
+const isEqual = (password, confirmPassword) => {
+  return password === confirmPassword;
+};
+
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
