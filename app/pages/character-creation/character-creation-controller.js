32482b2bb5100aceaeefdde652bcb00c46b5bd79
Adnir Andrade <adnir_andrade@hotmail.com>
2023-05-11 20:29:28 -0300

    Can now select stat to change.

--- app/pages/character-creation/character-creation-controller.js
+++ app/pages/character-creation/character-creation-controller.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
 // The code is purposely disorganized to comply with the activity's requirements
 // Adding Listener onload
-window.addEventListener("load", function () {
-  document.getElementById("multiclass").addEventListener("change", function () {
-    const multiclassFields = document.querySelectorAll(".multiclass-field");
+window.addEventListener('load', function () {
+  document.getElementById('multiclass').addEventListener('change', function () {
+    const multiclassFields = document.querySelectorAll('.multiclass-field');
 
     for (let i = 0; i < multiclassFields.length; i++) {
       multiclassFields[i].disabled = !this.checked;
@@ -13,14 +13,7 @@ window.addEventListener("load", function () {
 });
 
 // Sorting alphabetically
-const races = [
-  "Human", 
-  "Dwarf", 
-  "Elf", 
-  "Orc", 
-  "Malit", 
-  "Ramadriin"
-];
+const races = ['Human', 'Dwarf', 'Elf', 'Orc', 'Malit', 'Ramadriin'];
 
 races.sort(function (firstRace, secondRace) {
   if (firstRace < secondRace) {
@@ -33,12 +26,12 @@ races.sort(function (firstRace, secondRace) {
 });
 
 races.forEach((race) => {
-  const option = document.createElement("option");
+  const option = document.createElement('option');
 
   option.text = race;
   option.value = race.toLowerCase();
 
-  const raceDropdown = document.getElementById("race-dropdown");
+  const raceDropdown = document.getElementById('race-dropdown');
 
   raceDropdown.add(option);
 });
@@ -46,7 +39,7 @@ races.forEach((race) => {
 // Dynamically creating options for a dropdown using a function that calls another function
 function createOptions(firstValue, lastValue, selectDropdown, select) {
   for (let i = firstValue; i <= lastValue; i++) {
-    const option = document.createElement("option");
+    const option = document.createElement('option');
 
     option.value = i;
     option.textContent = i;
@@ -61,12 +54,12 @@ function selectDropdownById(selectId) {
   return document.getElementById(selectId);
 }
 
-createOptions(1, 20, selectDropdownById, "character-dropdown");
-createOptions(1, 10, selectDropdownById, "level-dropdown");
+createOptions(1, 20, selectDropdownById, 'character-dropdown');
+createOptions(1, 10, selectDropdownById, 'level-dropdown');
 
 // Making a mistake, aka, using an ID to select an element. thisIsTotallyNotRecommended is an ID inside the HTML
 for (let i = 1; i <= 10; i++) {
-  const option = document.createElement("option");
+  const option = document.createElement('option');
 
   option.value = i;
   option.textContent = i;
@@ -75,27 +68,27 @@ for (let i = 1; i <= 10; i++) {
 
 // Auto-executable function that I need to refine
 (function () {
-  document.addEventListener("keydown", function (event) {
-    if (event.code === "ArrowUp") {
+  document.addEventListener('keydown', function (event) {
+    if (event.code === 'ArrowUp') {
       userInput.focus();
     }
   });
 })();
 
 // Cool input that somehow worked
-const userInput = document.getElementsByClassName("character-name-input")[0];
+const userInput = document.getElementsByClassName('character-name-input')[0];
 
-userInput.addEventListener("input", () => {
+userInput.addEventListener('input', () => {
   userInput.style.width = `${userInput.value.length * 27}px`;
 });
 
 // Focus and Blur sexy events
-userInput.addEventListener("focus", () => {
-  userInput.style.fontSize = 30 + "px";
+userInput.addEventListener('focus', () => {
+  userInput.style.fontSize = 30 + 'px';
 });
 
-userInput.addEventListener("blur", () => {
-  userInput.style.fontSize = 28 + "px";
+userInput.addEventListener('blur', () => {
+  userInput.style.fontSize = 28 + 'px';
 });
 
 // Stat screen
@@ -107,20 +100,31 @@ userInput.addEventListener("blur", () => {
 //   console.log("Test!");
 // }
 
-  const form1 = document.getElementById('name-form');
+const form1 = document.getElementById('name-form');
 
-  const form2 = document.getElementById('character-form');
+const form2 = document.getElementById('character-form');
 
-  const form3 = document.getElementById('stats-form');
+const form3 = document.getElementById('stats-form');
 
-  const form4 = document.getElementById('perks-form');
+const form4 = document.getElementById('perks-form');
 
-  const submitBtn = document.getElementById('submitBtn');
+const submitBtn = document.getElementById('submitBtn');
 
-  submitBtn.addEventListener('click', (event) => {
-    event.preventDefault();
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
 
-    form1.blur();
+  form1.blur();
 
-    console.log("It worked!");
-  })
+  console.log('It worked!');
+});
+
+// Second screen
+const statSquares = document.getElementsByClassName('stat-bg');
+
+const selectedSquare = document.getElementById('selected-stat');
+
+for (let i = 0; i < statSquares.length; i++) {
+  statSquares[i].addEventListener('click', (event) => {
+    selectedSquare.textContent = statSquares[i].textContent;
+  });
+}
