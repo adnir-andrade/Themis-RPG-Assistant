69dc535e7e183201d6e908750cc5f6d7f8b0d174
Adnir Andrade <adnir_andrade@hotmail.com>
2023-05-26 21:34:20 -0300

    Refactored

--- app/pages/campaign-view/campaign-view-controller.js
+++ app/pages/campaign-view/campaign-view-controller.js
@@ -1,43 +1,50 @@
-"use strict";
+'use strict';
 
-const characterButton = document.getElementById("character-1");
+const characterButton = document.getElementById('character-1');
 
 const originalPosition = characterButton.offsetLeft;
 
 const newPosition = (function () {
   const OFFSET = 0.4;
-  
+
   return originalPosition - originalPosition * OFFSET;
 })();
 
 let isSelected = false;
-$(".hidden-content").hide();
+$('.hidden-content').hide();
 
-$(".button-slider").on( "click", function() {
-  if ($(this).next().is( ":hidden" ) ) {
-    $(this).next().slideDown( "slow" );
+$('.button-slider').on('click', function () {
+  if ($(this).next().is(':hidden')) {
+    $(this).next().slideDown('slow');
   } else {
-    $(this).next().slideUp( "slow" );
+    $(this).next().slideUp('slow');
   }
 });
 
-
 // Fancy Buttons again, but this with jQuery. TODO: Refactor to Util
-$(".custom-button").each(function() {
-  $(this).on("mouseover", () => {
+$('.custom-button').each(function () {
+  $(this).on('mouseover', () => {
     $(this).css({
-      "mix-blend-mode": "overlay",
-      "background": "radial-gradient(circle at center, rgba(255, 255, 255, 0.9) 0%, rgba(255, 255, 255, 0) 70%)"
+      'mix-blend-mode': 'overlay',
+      background:
+        'radial-gradient(circle at center, rgba(255, 255, 255, 0.9) 0%, rgba(255, 255, 255, 0) 70%)',
     });
   });
 });
 
-$(".custom-button").each(function() {
-  $(this).on("mouseout", () => {
+$('.custom-button').each(function () {
+  $(this).on('mouseout', () => {
     $(this).css({
-      "mix-blend-mode": "",
-      "background": ""
-    })
+      'mix-blend-mode': '',
+      background: '',
+    });
   });
 });
 
+const modalTrigger = function () {
+  uglipop({
+    class: 'test-modal',
+    source: 'div', //html?
+    content: 'modalDemo',
+  });
+};
