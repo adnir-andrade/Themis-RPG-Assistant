6a0726906498393b5754d382d2ce4c3c64e156bb
Adnir Andrade <adnir_andrade@hotmail.com>
2023-06-01 10:44:35 -0300

    Mongoose experiment

---
+++ app/models/seeds.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Character = require('./app/models/character');
+
+// getting-started.js
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/themysDb');
+  console.log('Mongoose Connected');
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const seedling = new Character({
+  name: 'Lethnox Keturak',
+  characterLevel: 12,
+  race: 'Dwarf',
+  baseClass: 'Paladin',
+});
